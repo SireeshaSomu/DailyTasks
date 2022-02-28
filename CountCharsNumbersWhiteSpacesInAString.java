ebf768d67ebd26f93dfccf5c9f079126ab2fcf42
SireeshaSomu <sireesha.somu@gmail.com>
2022-02-28 13:10:27 -0600

    Code for counting the chars, numbers and special characters in a string

--- CountCharsNumbersWhiteSpacesInAString.java
+++ CountCharsNumbersWhiteSpacesInAString.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CountCharsNumbersWhiteSpacesInAString {
-	public boolean count(String str) {
+	public String count(String str) {
 		int lower = 0, number = 0, whitespaces = 0;
 
 		for (int i = 0; i < str.length(); i++) {
@@ -17,18 +17,17 @@ public class CountCharsNumbersWhiteSpacesInAString {
 				whitespaces++;
 		}
 
-		System.out.println("Lower case letters: " + lower + "  Number:  " + number + "  White Spaces: " + whitespaces);
-		return true;
+		return String.valueOf(lower) + " " + String.valueOf(number) + " " + String.valueOf(whitespaces);
 	}
 
 	@Test
 	public void test1Count() {
-		Assert.assertTrue(count("abc ef4 5$"));
+		Assert.assertEquals(count("abc ef4 5$"), "5 2 2");
 	}
 
 	@Test
 	public void test2Count() {
-		Assert.assertTrue(count("390a\t\t 4cbef\n z$"));
+		Assert.assertTrue(count("390a\t\t 4cbef\n z$").equals("6 4 5"));
 	}
 
 }
