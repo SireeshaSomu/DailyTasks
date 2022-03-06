064615d40388d00d9308d9e667579c08ef83b5df
SireeshaSomu <sireesha.somu@gmail.com>
2022-03-05 22:00:32 -0600

    Updated project with changes

--- cucumber-jpmorgan/src/test/java/test/TestRunner.java
+++ cucumber-jpmorgan/src/test/java/test/TestRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions" }, plugin = { "pretty",
+		"html:target/html", "json:target/report.json" })
 public class TestRunner {
 
 }
